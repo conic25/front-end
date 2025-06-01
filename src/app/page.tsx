d9422eb109ace44b8f99e01836dede971bb8ed06
유림 <yulimh001@gmail.com>
2025-06-02 06:42:48 +0900

    feat: pretter, eslint 설치

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-import Button1 from "@/components/common/Button1";
+import Button1 from '@/components/common/Button1'
 
 export default function Home() {
   return (
-      <>
-        <button className={'text-5xs'}>히히</button>
-        <button className={'text-2xs'}>히히</button>
+    <>
+      <button className={'text-5xs'}>히히</button>
+      <button className={'text-2xs'}>히히</button>
 
-        <Button1 label={'헿'}></Button1>
-      </>
-  );
+      <Button1 label={'헿'}></Button1>
+    </>
+  )
 }
--- src/components/storybook/Button.stories.tsx
+++ src/components/storybook/Button.stories.tsx
@@ -1,34 +1,34 @@
 // Button.stories.tsx
-import type { Meta, StoryObj } from '@storybook/nextjs';
-import Button1 from "@/components/common/Button1";
+import type { Meta, StoryObj } from '@storybook/nextjs'
+import Button1 from '@/components/common/Button1'
 
 const meta: Meta = {
-    title: 'Components/Button',
-    component: Button1,
-    argTypes: {
-        type: {
-            control: 'select',
-            options: ['primary', 'secondary', 'outline', 'outline2', 'ghost'],
-        },
-        status: {
-            control: 'select',
-            options: ['default', 'hover', 'click', 'disabled'],
-        },
-        size: {
-            control: 'select',
-            options: ['lg', 'md', 'sm'],
-        },
+  title: 'Components/Button',
+  component: Button1,
+  argTypes: {
+    type: {
+      control: 'select',
+      options: ['primary', 'secondary', 'outline', 'outline2', 'ghost'],
     },
-};
+    status: {
+      control: 'select',
+      options: ['default', 'hover', 'click', 'disabled'],
+    },
+    size: {
+      control: 'select',
+      options: ['lg', 'md', 'sm'],
+    },
+  },
+}
 
-export default meta;
-type Story = StoryObj;
+export default meta
+type Story = StoryObj
 
 export const Default: Story = {
-    args: {
-        label: '버튼',
-        type: "ghost",
-        size: 'md',
-        status: "disabled",
-    },
-};
+  args: {
+    label: '버튼',
+    type: 'ghost',
+    size: 'md',
+    status: 'disabled',
+  },
+}
